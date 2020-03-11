65919044a273ada115957d1a94c2ea77a4c12291
Mauricio Garcia <maugarciaflores@gmail.com>
2020-03-10 18:16:39 -0600

    Update Tools.java

--- EmployeeMeeting/app/src/main/java/com/maugarciaf/employeemeeting/utils/Tools.java
+++ EmployeeMeeting/app/src/main/java/com/maugarciaf/employeemeeting/utils/Tools.java
@@ -1,4 +1,28 @@
 package com.maugarciaf.employeemeeting.utils;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+import android.view.Window;
+import android.view.WindowManager;
+
 public class Tools {
+
+    public static void setSystemBarColor(Activity act) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = act.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(act.getResources().getColor(R.color.colorPrimaryDark));
+        }
+    }
+
+    public static void setSystemBarTransparent(Activity act) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = act.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
 }

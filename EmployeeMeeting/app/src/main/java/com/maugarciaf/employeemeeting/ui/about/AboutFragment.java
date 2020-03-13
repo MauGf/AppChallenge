5569722b9b60b457b960948baa172f3f60dd277c
MauGf <maugarciaflores@gmail.com>
2020-03-12 22:19:48 -0600

    Update AboutFragment.java

--- EmployeeMeeting/app/src/main/java/com/maugarciaf/employeemeeting/ui/about/AboutFragment.java
+++ EmployeeMeeting/app/src/main/java/com/maugarciaf/employeemeeting/ui/about/AboutFragment.java
@@ -16,20 +16,13 @@ import com.maugarciaf.employeemeeting.R;
 
 public class AboutFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_about, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
